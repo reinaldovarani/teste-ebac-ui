6854e04671dfc8c78896148c3dcafa2d05986133
Reinaldo Varani <reinaldovarani@yahoo.com.br>
2023-02-20 20:33:33 -0300

    usando custom commands

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -15,18 +15,27 @@ describe('Funcionalidade Página de produtos', () => {
             .click()
     });
 
-    it.only('Deve adiciobar um produto ao carrinho', () => {
-        var quantidade =10
+    it('Deve adiciobar um produto ao carrinho', () => {
+        var quantidade = 10
 
         cy.get('[class="product-block grid"]')
-        .contains('Abominable Hoodie').click()
+            .contains('Abominable Hoodie').click()
         cy.get('.button-variable-item-M').click()
         cy.get('.button-variable-item-Green').click()
+
         cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
 
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
-        cy.get('.woocommerce-message').should('contain' , quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Black', 2)
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Abominable Hoodie', 'XS', 'Red', 5)
     });
 
 });
